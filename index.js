475d1e0f6f8652487c904caabe74e597aa4238c6
danielshires <hello@danielshires.com>
2022-05-29 20:54:53 +1000

    First commit

---
+++ index.js
@@ -0,0 +1,193 @@
+let baseCurrency = 'AUD'
+let targetCurrency = 'GBP'
+let convertCurrencyAPI
+
+const inputBaseCurrency = document.querySelector('input#baseCurrency')
+const selectBaseCurrency = document.querySelector('select#selectBaseCurrency')
+const inputTargetCurrency = document.querySelector('input#targetCurrency')
+const selectTargetCurrency = document.querySelector('select#selectTargetCurrency')
+const conversionText = document.querySelector('div#conversionText')
+const conversionDate = document.querySelector('div#conversionDate')
+const button = document.querySelector('button')
+
+// Functions
+
+function getResults() {
+
+    convertCurrencyAPI = `https://api.exchangerate.host/convert?from=${baseCurrency}&to=${targetCurrency}`
+    toggleLoading(true)
+    fetch(convertCurrencyAPI).then(currency => currency.json()).then(displayResults).catch(error => {
+        toggleLoading(false)
+        return error
+    })
+
+
+}
+
+function getCurrencyInfo() {
+
+    const currencySymbolsAPI = `https://api.exchangerate.host/symbols`
+
+    fetch(currencySymbolsAPI).then(currencies => currencies.json()).then(populateDropDown).catch(error => {
+        return error
+    })
+}
+
+function displayResults(currency) {
+
+
+    // Runs the conversion functions when changing the input
+    inputBaseCurrency.addEventListener('input', baseConversion)
+    inputTargetCurrency.addEventListener('input', targetConversion)
+
+    // Selects the base rate
+    let baseRate = currency.result
+
+    // The two different exchange rates, to 6 decimnal points then converts to number
+    let currencyFrom = parseFloat(baseRate.toFixed(6))
+    let currencyTo = parseFloat((1 / baseRate).toFixed(6))
+
+    // Stores currency exchange rates and input data
+    const currencyData = {
+        baseRate: currencyTo,
+        targetRate: currencyFrom,
+        baseInput: 0,
+        targetInput: 0,
+    }
+
+    // Function to convert input number with the target currency
+    function multiplyRate(input, target) {
+        return input * target
+    }
+
+    // Changes the input values based on exchange rate
+    function baseConversion() {
+
+        // Sets object data to the input
+        currencyData.baseInput = parseFloat(inputBaseCurrency.value)
+
+        // Takes the return value from the multiplay rate function
+        const baseToTarget = parseFloat(multiplyRate(currencyData.baseInput, currencyData.targetRate).toFixed(2))
+            // Updates the target input
+        inputTargetCurrency.value = baseToTarget
+            // Updates the currency text
+        const dateOfData = currency.date
+        const newDate = new Date(dateOfData)
+        const newDateString = newDate.toString().slice(3, 15)
+        conversionText.innerHTML = `1 ${baseCurrency} = ${currencyFrom} ${targetCurrency}`
+        conversionDate.innerHTML = `Data accurate as of ${newDateString}`
+    }
+
+    function targetConversion() {
+
+        currencyData.targetInput = parseFloat(inputTargetCurrency.value)
+
+        const targetToBase = parseFloat(multiplyRate(currencyData.targetInput, currencyData.baseRate)).toFixed(2)
+        inputBaseCurrency.value = targetToBase
+    }
+
+    // Runs the initial base conversion
+    baseConversion()
+    setTimeout(function() {
+        toggleLoading(false)
+    }, 500)
+}
+
+function populateDropDown(currencies) {
+
+    // Assign data from API to an array
+    const symbolsObject = currencies.symbols
+
+    // Loop through array and create option elements
+    for (const currency in symbolsObject) {
+        // Update Base
+        const optionElBase = document.createElement('option')
+        selectBaseCurrency.append(optionElBase)
+        optionElBase.className = 'baseCurrency'
+        optionElBase.value = symbolsObject[currency].code
+        optionElBase.innerHTML = symbolsObject[currency].code
+
+        // Update Target
+        const optionElTarget = document.createElement('option')
+        selectTargetCurrency.append(optionElTarget)
+        optionElTarget.className = 'targetCurrency'
+        optionElTarget.value = symbolsObject[currency].code
+        optionElTarget.innerHTML = symbolsObject[currency].code
+    }
+
+    const optionBase = document.querySelectorAll('option.baseCurrency')
+    const optionTarget = document.querySelectorAll('option.targetCurrency')
+
+    // Apply 'selected' to the default base currency
+    optionBase.forEach(e => {
+        if (e.innerHTML === baseCurrency) {
+            e.setAttribute('selected', 'selected')
+        }
+    })
+
+    // Apply 'selected' to the default target currency
+    optionTarget.forEach(e => {
+        if (e.innerHTML === targetCurrency) {
+            e.setAttribute('selected', 'selected')
+        }
+    })
+
+    updateCurrencyList()
+}
+
+function switchCurrencies() {
+
+    const button = document.querySelector('button')
+
+    button.addEventListener('click', function(e) {
+        selectBaseCurrency.value = targetCurrency
+        selectTargetCurrency.value = baseCurrency
+        baseCurrency = selectBaseCurrency.value
+        targetCurrency = selectTargetCurrency.value
+        getResults()
+    })
+}
+
+function updateCurrencyList() {
+    // Change the default base currency when updating the dropdown input
+    selectBaseCurrency.addEventListener('change', function() {
+        const selectedBaseCurrency = selectBaseCurrency.options[selectBaseCurrency.selectedIndex].text
+        baseCurrency = selectedBaseCurrency
+            // Run the conversion function
+        getResults()
+    })
+
+    // Change the default target currency when updating the dropdown input
+    selectTargetCurrency.addEventListener('change', function() {
+        const selectedTargetCurrency = selectTargetCurrency.options[selectTargetCurrency.selectedIndex].text
+        targetCurrency = selectedTargetCurrency
+            // Run the conversion function
+        getResults()
+    })
+}
+
+function toggleLoading(state) {
+    const loadingIcon = document.querySelector('#loading')
+        // In here we toggle the page loading state between loading and not-loading
+        // if our state is true, we add a loading class to body
+    if (state) {
+        loadingIcon.style.display = 'block';
+    } else {
+        loadingIcon.style.display = 'none';
+    }
+}
+
+
+// Animation
+button.addEventListener('click', function(e) {
+    e.target.classList.add('animationActive')
+    setTimeout(function() {
+        e.target.classList.remove('animationActive')
+    }, 500)
+})
+
+// Start program
+
+getResults()
+getCurrencyInfo()
+switchCurrencies()
---
+++ mvp.md
@@ -0,0 +1,25 @@
+## Planning
+
+Plan out your features
+Develop user stories
+“As [ a user ], I want [ to perform this action ] so that [ I can accomplish this goal ].”
+Features should not need you there to explain them to users
+Plan out the structure of your JSON requests
+Project Pitches
+
+Before you start working on your project, you'll pitch your project idea to your instructors for approval and feedback.
+
+## For your project pitch, you should include:
+
+The basic story of your application
+The core features of your MVP
+The API data you'll be using and how you'll use it
+Challenges you expect to face
+How you are meeting the requirements of the project
+Feel free to send this pitch to your instructor via slack asynchronously.
+
+## MVP ASAP
+
+Build a Minimum Viable Product (MVP) as quickly as possible.
+Pick an API and explore it early on to ensure it will work for your need
+
